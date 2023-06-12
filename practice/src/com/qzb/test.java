8a3056f3c433f6144da992d8a03ae449791f1ce9
SidddMeier <qzb200011@163.com>
2023-06-12 20:33:04 +0800

    Changes

--- practice/src/com/qzb/test.java
+++ practice/src/com/qzb/test.java
@@ -1,8 +1,12 @@
 package com.qzb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class test {
     public static void main(String[] args) {
         String s1 = new String("123");
         System.out.println(s1.compareTo("1234"));
+        List l = new ArrayList();
     }
 }
