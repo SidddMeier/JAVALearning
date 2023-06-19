7c0e743099fce1985019da1cb9484374d82c8662
SidddMeier <qzb200011@163.com>
2023-06-19 17:46:18 +0800

    Changes

--- practice/src/com/qzb/set/Test.java
+++ practice/src/com/qzb/set/Test.java
@@ -1,7 +1,9 @@
 package com.qzb.set;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Test {
     public static void main(String[] args) {
@@ -9,7 +11,8 @@ public class Test {
         TestSet<String> b = new TestSet<>();
         List<String>c = new ArrayList();
         CompareTo i = new TestSet<String>();
-
-
+        Student student = new Student();
+        Student student1 = new Student();
+        Map<Integer,String> map = new HashMap<>();
     }
 }
